0aee7ac3f698c977475d3aa94cb30c0fc5e8e5b8
GodsonAjieh-CAL <Godsona@calastone.com>
2026-09-21 13:58:59 +0100

    Added OrderService

---
+++ ETF Orders Domain API/OrderService.cs
@@ -0,0 +1,25 @@
+﻿namespace ETF_Orders_Domain_API
+{
+    public class OrderService : IOrderService
+    {
+        public Task<string> AddOrders(int id, string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DeleteOrders(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetOrders(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> UpdateOrders(int id, string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
