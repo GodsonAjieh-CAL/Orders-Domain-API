490b939d3c345708a12d72fe48a897e462679de4
GodsonAjieh-CAL <Godsona@calastone.com>
2026-09-21 13:56:39 +0100

    Add project files.

---
+++ ETF Orders Domain API/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿namespace ETF_Orders_Domain_API
+{
+    public interface IOrderService
+    {
+        public Task<string> GetOrders(string name);
+
+        public Task<string> AddOrders(int id, string name);
+        public Task<string> UpdateOrders(int id, string name);
+        public Task<string> DeleteOrders(int id);
+
+    }
+}
