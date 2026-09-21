6a32ed32c2a67569d590ee39c06db3246829bf7e
GodsonAjieh-CAL <Godsona@calastone.com>
2026-09-21 14:21:36 +0100

    Downloaded nuget package swashbuckle, now I can add swaggergen to project.

---
+++ ETF Orders Domain API/OrdersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ETF_Orders_Domain_API
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrdersController : ControllerBase
+    {
+        [HttpGet(Name = "GetOrders")]
+        public async Task<IActionResult> GetOrders(string name)
+        {
+            // Implementation for getting orders
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddOrders(int id, string name)
+        {
+            // Implementation for adding orders
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateOrders(int id, string name)
+        {
+            // Implementation for updating orders
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrders(int id)
+        {
+            // Implementation for deleting orders
+            return NoContent();
+        }
+    }
+}
--- ETF Orders Domain API/Program.cs
+++ ETF Orders Domain API/Program.cs
@@ -1,8 +1,18 @@
+using ETF_Orders_Domain_API;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IOrderService, OrderService>();
 
 var app = builder.Build();
 
@@ -15,6 +25,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
+
+app.MapControllers();
 
 app.Run();
 
